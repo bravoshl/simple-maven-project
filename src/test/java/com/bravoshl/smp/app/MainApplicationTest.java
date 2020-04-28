d79ab715cc3f7cb779885e09eed3dc04accfb123
bravoshl <bravoshl@hotmail.com>
2020-04-28 10:42:47 +0800

    add test unit

---
+++ src/test/java/com/bravoshl/smp/app/MainApplicationTest.java
@@ -0,0 +1,38 @@
+package com.bravoshl.smp.app;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class MainApplicationTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testAppMain() {
+        MainApplication.main(null);
+        try {
+            assertEquals("hello world" + System.getProperty("line.separator"), outContent.toString());
+        } catch (AssertionError e) {
+            fail("\"message\" is not \"Hello World!\"");
+        }
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+
+}
